a8e48a68599fb55d715de78af19839a83643768a
aleksey <loliamalexxaxa@gmail.com>
2022-11-16 16:41:27 +0400

    should be saved

--- notan_ui/src/text.rs
+++ notan_ui/src/text.rs
@@ -28,6 +28,7 @@ pub struct Text<State: UIStateCl> {
     pub align_v: AlignVertical,
     pub pos: Position,
     pub size: f32,
+    pub rect_size: Option<Size>,
     pub max_width: Option<f32>,
     pub color: Color,
     pub boo: PhantomData<State>
@@ -35,28 +36,33 @@ pub struct Text<State: UIStateCl> {
 impl<State: UIStateCl> Form<State> for Text<State> {
     fn draw(&mut self, app: &mut App, gfx: &mut Graphics, plugins: &mut Plugins, state: &mut State) {
         let font = Access::<Vec<Font>>::get_mut(state)
-            .get(self.font.0)
+           .get(self.font.0)
             .expect(&*format!("Cant find font with index {}", self.font.0)).clone();
         let draw = Access::<Draw>::get_mut(state);
-        let mut text_builder = draw.text(&font, &*self.text);
-        let mut text_builder = match self.align_h {
-            AlignHorizontal::Left => text_builder.h_align_left(),
-            AlignHorizontal::Center => text_builder.h_align_center(),
-            AlignHorizontal::Right => text_builder.h_align_right()
-        };
-        text_builder = match self.align_v {
-            AlignVertical::Bottom => text_builder.v_align_bottom(),
-            AlignVertical::Center => text_builder.v_align_middle(),
-            AlignVertical::Top => text_builder.v_align_top()
-        };
-        let text_size = self.get_size();
-        let mut pos = self.pos;
-        text_builder
-            .position(pos.0, pos.1)
-            .size(self.size)
-            .color(self.color);
-        if let Some(width) = self.max_width { text_builder.max_width(width); }
-    }
+        {
+            let mut text_builder = draw.text(&font, &*self.text);
+            let mut text_builder = match self.align_h {
+                AlignHorizontal::Left => text_builder.h_align_left(),
+                AlignHorizontal::Center => text_builder.h_align_center(),
+                AlignHorizontal::Right => text_builder.h_align_right()
+            };
+            text_builder = match self.align_v {
+                AlignVertical::Bottom => text_builder.v_align_bottom(),
+                AlignVertical::Center => text_builder.v_align_middle(),
+                AlignVertical::Top => text_builder.v_align_top()
+            };
+            let text_size = self.get_size();
+            let mut pos = self.pos;
+            text_builder
+                .position(pos.0, pos.1)
+                .size(self.size)
+                .color(self.color);
+            if let Some(width) = self.max_width { text_builder.max_width(width); }
+        }
+        if self.rect_size == None {
+            let bounds = draw.last_text_bounds();
+            self.rect_size = Some(Size(bounds.width, bounds.height));
+    }   }
     fn after(&mut self, app: &mut App, gfx: &mut Graphics, plugins: &mut Plugins, state: &mut State) {}
 }
 impl<State: UIStateCl> Positionable for Text<State> {
@@ -67,14 +73,17 @@ impl<State: UIStateCl> Positionable for Text<State> {
         self.pos+=to_add;
     }
     fn get_size(&self) -> Size {
-        if let Some(width) = self.max_width {
-            return (width, if width != 0. {
-                self.size * (self.text.chars().count() as f32) / width * self.size
-            } else {
-                self.size
-            }).into();
+        let mut size = Size(0., 0.);
+        if let Some(rect_size) = self.rect_size {
+            size = rect_size;
         }
-        (self.size * (self.text.chars().count() as f32), self.size).into()
+        if let Some(width) = self.max_width {
+            size.0 = width;
+        }
+        if size == Size(0., 0.) && self.text.chars().count() > 0 {
+            return Size(self.size * (self.text.chars().count() as f32), self.size)
+        }
+        size
     }
     fn get_pos(&self) -> Position { self.pos }
 }
@@ -87,6 +96,7 @@ impl<State: UIStateCl> Default for Text<State> {
             align_v: AlignVertical::Top,
             pos: Position(0., 0.),
             size: 0.,
+            rect_size: None,
             max_width: None,
             color: Color::BLACK,
             boo: PhantomData
@@ -100,7 +110,15 @@ impl<State: UIStateCl> Text<State> {
                max_width: Option<f32>,
                color: Color) -> Self {
         Self {
-            text: text.into(), font: font.into(), align_h, align_v, pos, size, max_width, color,
+            text: text.into(),
+            font: font.into(),
+            align_h,
+            align_v,
+            pos,
+            size,
+            rect_size: None,
+            max_width,
+            color,
             boo: PhantomData
 }   }   }
 
