78cadb384a07c9998a585be9f30fae61e554e79e
aleksey <loliamalexxaxa@gmail.com>
2023-09-10 03:18:02 +0400

    aaa'

---
+++ advini/src/lib.rs
@@ -0,0 +1,375 @@
+use std::{collections::{HashMap}, str::Chars, fmt::{Display, Debug}};
+use ini_core::{Item, Parser};
+use num::Num;
+pub use advini_derive::*;
+
+pub fn parse_for_sections(ini_doc: &str) -> Vec<(String, HashMap<String, String>)> {
+    let mut result = Vec::new();
+    let mut old_sec = "";
+	let mut last_prop = "".into();
+    let mut props: HashMap<String, String> = HashMap::new();
+    let parser = Parser::new(&*ini_doc).auto_trim(true);
+    for item in parser {
+        match item {
+            Item::Section(sec) => {
+                if !old_sec.is_empty() {
+                    result.push((old_sec.into(), props));
+                    props = HashMap::new();
+                    old_sec = sec;
+                } else {
+                    old_sec = sec
+                }
+            }
+            Item::Property(k, v) => {
+				let prop = k.to_lowercase().to_string();
+                props.insert(prop.clone(), v.into());
+				last_prop = prop;
+            }
+            Item::Blank | Item::Comment(_) => {},
+			Item::Action(v) => {
+				if let Some(old) = props.get_mut(&last_prop) {
+					old.push_str(v);
+				};
+			},
+            Item::Error(err) => panic!("{}", err),
+        }
+    }
+    result.push((old_sec.into(), props));
+	result
+}
+pub const SEPARATOR: char = ',';
+pub fn parse_string_from_string<'a>(mut chars: Chars<'a>) -> Result<(String, Chars<'a>), IniParseError> {
+	let mut level = (0, false);
+	let mut initial_level = 0;
+	let mut result_string = String::new();
+	loop {
+		if let Some(chr) = chars.next() {
+			match (level.1, chr) {
+				(false, '"') => {
+					level.0 += 1;
+					initial_level = level.0;
+					continue;
+				},
+				(false, _) => {
+					level.1 = true;
+				},
+				(true, '"') => {
+					level.0 -= 1;
+					if let Some(chr) = chars.next() {
+						if chr == '"' {
+							if level.0 == 0 {
+								result_string.push('"');
+							}
+							level.0 = 0.max(level.0 - 1);
+						} else {
+							if chr == SEPARATOR && level.0 == 0 {
+								break
+							} else {
+								result_string.push('"');
+								result_string.push(chr);
+								level.0 = initial_level;
+							} 
+						}
+						continue;
+					}
+					continue;
+				},
+				(true, SEPARATOR) => {
+					if level.0 == 0 {
+						break;
+					}
+				}
+				(_, _) => {}
+			}
+			result_string.push(chr);
+		} else {
+			if result_string.is_empty() {
+				return Err(IniParseError::Empty(chars));
+			} else {
+				break;
+			};
+		}
+	}
+	Ok((result_string, chars))
+		
+}
+
+#[derive(Debug)]
+pub enum IniParseError<'a> {
+	Error(&'a str),
+	Empty(Chars<'a>)
+}
+impl From<IniParseError<'static>> for &'static str {
+	fn from(value: IniParseError<'static>) -> Self {
+		match value {
+			IniParseError::Error(string) => string,
+			IniParseError::Empty(_) => "just no chars"
+		}
+	}
+}
+impl Display for IniParseError<'_> {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		f.write_str(match self {
+			IniParseError::Error(string) => string,
+			IniParseError::Empty(_) => "just no chars"
+		})
+	}
+}
+impl From<&'static str> for IniParseError<'_> {
+	fn from(value: &'static str) -> Self {
+		Self::Error(value)
+	}
+}
+pub trait Sections where Self: Sized {
+	fn from_section(sec: HashMap<String, String>) -> Result<(Self, HashMap<String, String>), &'static str>;
+	fn to_section(&self) -> HashMap<String, String>;
+}
+pub trait Ini where Self: Sized {
+	fn eat<'a>(chars: Chars<'a>) -> Result<(Self, Chars<'a>), IniParseError>;
+	fn vomit(&self) -> String;
+}
+impl Ini for String {
+	fn eat<'a>(chars: Chars<'a>) -> Result<(Self, Chars<'a>), IniParseError> {
+		parse_string_from_string(chars)
+	}
+	fn vomit(&self) -> String {
+		let amount = self
+			.chars()
+			.fold((0, 0, false), |acc, chr|
+				  match (chr, acc.2) {
+					  ('"', true) => {
+						  (acc.0, acc.1 + 1, true)
+					  },
+					  (_, true) => {
+						  (acc.1, 0, false)
+					  },
+					  ('"', false) => {
+						  (acc.0, 1, true)
+					  },
+					  (_,_) => { acc }
+				  }
+			).0;
+		let beginning = (0..=amount).map(|_| "\"").collect::<Vec<&str>>().concat();
+		let mut res = beginning.clone();
+		res.push_str(self.as_str());
+		res.push_str(beginning.as_str());
+		res
+	}
+}
+impl Ini for bool {
+	fn eat<'a>(mut chars: Chars<'a>) -> Result<(Self, Chars<'a>), IniParseError> {
+		if let Some(chr) = chars.next() {
+			loop {
+				if chars.next() == Some(SEPARATOR) {
+					break;
+				}
+			}
+			match chr {
+				SEPARATOR => {
+					return Err(IniParseError::Empty(chars))
+				},
+				't' | 'y' | '1' => {
+					Ok((true, chars))
+				},
+				'f' | 'n' | '0' => {
+					Ok((false, chars))
+				},
+				_ => Err(IniParseError::Error("what"))
+			}
+		} else {
+			Err(IniParseError::Empty(chars))
+		}
+	}
+	fn vomit(&self) -> String {
+		if *self {
+			"true".into()
+		} else {
+			"false".into()
+		}
+	}
+}
+// impl<'b> Ini for &'b str {
+// 	fn eat<'a>(chars: Chars<'a>) -> Result<(Self, Chars<'a>), IniParseError> {
+// 		match parse_string_from_string(chars) {
+// 			Ok(v) => Ok((v.0.as_str(), v.1)),
+// 			Err(a) => Err(a)
+// 		}
+// 	}
+// 	fn vomit(&self) -> String {
+// 		let amount = self
+// 			.chars()
+// 			.fold((0, 0, false), |acc, chr|
+// 				  match (chr, acc.2) {
+// 					  ('"', true) => {
+// 						  (acc.0, acc.1 + 1, true)
+// 					  },
+// 					  (_, true) => {
+// 						  (acc.1, 0, false)
+// 					  },
+// 					  ('"', false) => {
+// 						  (acc.0, 1, true)
+// 					  },
+// 					  (_,_) => { acc }
+// 				  }
+// 			).0;
+// 		let beginning = (0..=amount).map(|_| "\"").collect::<Vec<&str>>().concat();
+// 		let mut res = beginning.clone();
+// 		res.push_str(self);
+// 		res.push_str(beginning.as_str());
+// 		res
+// 	}
+// }
+impl Ini for char {
+	fn eat<'a>(mut chars: Chars<'a>) -> Result<(Self, Chars<'a>), IniParseError> {
+		Ok(
+			({
+				let chr = match chars.next() {
+					Some(v) => v,
+					None => return Err(IniParseError::Empty(chars))
+ 				};
+				if chars.next() != Some(SEPARATOR) {
+					return Err("problems with your char".into());
+				}
+				chr
+			},
+			chars)
+		)
+	}
+	fn vomit(&self) -> String {
+		self.to_string()
+	}
+}
+
+macro_rules! impl_for_num {
+	($ty:ty) => {
+		impl Ini for $ty {
+			fn eat<'a>(mut chars: Chars<'a>) -> Result<(Self, Chars<'a>), IniParseError> {
+				let mut str_repr = String::new();
+				loop {
+					if let Some(chr) = chars.next() {
+						if chr != SEPARATOR {
+							str_repr.push(chr);
+						} else {
+							return Ok((Num::from_str_radix(&str_repr.trim(), 10).map_err(|_| "Parsing of num failed")?, chars));
+						}
+					} else {
+						return if str_repr.is_empty() {
+							Err(IniParseError::Empty(chars))
+						} else {
+							Ok((Num::from_str_radix(&str_repr.trim(), 10).map_err(|_| "Parsing of num failed")?, chars))
+						}
+					}
+				}
+			}
+			fn vomit(&self) -> String {
+				self.to_string()
+			}
+		}
+	}
+}
+impl_for_num!(i128);
+impl_for_num!(i64);
+impl_for_num!(i32);
+impl_for_num!(i16);
+impl_for_num!(i8);
+impl_for_num!(u128);
+impl_for_num!(u64);
+impl_for_num!(u32);
+impl_for_num!(u16);
+impl_for_num!(u8);
+impl_for_num!(f32);
+impl_for_num!(f64);
+impl_for_num!(usize);
+impl_for_num!(isize);
+
+macro_rules! tuple_impls {
+    () => {};
+    (($idx:tt => $typ:ident), $( ($nidx:tt => $ntyp:ident), )*) => {
+        tuple_impls!([($idx, $typ);] $( ($nidx => $ntyp), )*);
+        tuple_impls!($( ($nidx => $ntyp), )*); // invoke macro on tail
+    };
+    ([$(($accIdx: tt, $accTyp: ident);)+]  ($idx:tt => $typ:ident), $( ($nidx:tt => $ntyp:ident), )*) => {
+      tuple_impls!([($idx, $typ); $(($accIdx, $accTyp); )*] $( ($nidx => $ntyp), ) *);
+    };
+
+    ([($idx:tt, $typ:ident); $( ($nidx:tt, $ntyp:ident); )*]) => {
+		impl<$typ : Ini, $( $ntyp : Ini ),*> Ini for ($typ, $( $ntyp ),*) {
+			fn eat<'a>(mut chars: Chars<'a>) -> Result<(Self, Chars<'a>), IniParseError> {
+				let result = (
+					{
+						let res;
+						(res, chars) = <$typ as Ini>::eat(chars)?;
+						res
+					},
+					$(
+						{
+							let res;
+							(res, chars) = <$ntyp as Ini>::eat(chars)?;
+							res
+						},
+					)*
+				);
+				Ok((result, chars))
+			}
+			fn vomit(&self) -> String {
+				[self.$idx.vomit(), $( self.$nidx.vomit() ), *].join(",")
+			}
+		}
+	}
+}
+tuple_impls!(
+    (9 => J),
+    (8 => I),
+    (7 => H),
+    (6 => G),
+    (5 => F),
+    (4 => E),
+    (3 => D),
+    (2 => C),
+    (1 => B),
+    (0 => A),
+);
+
+impl<T: Ini> Ini for Vec<T> {
+	fn eat<'a>(mut chars: Chars<'a>) -> Result<(Self, Chars<'a>), IniParseError> {
+		let mut new = Vec::new();
+		loop {
+			let value;
+			let res = T::eat(chars);
+			(value, chars) = match res {
+				Ok(v) => v,
+				Err(IniParseError::Empty(chars)) => return Ok((new, chars)),
+				err => return Err(err.err().unwrap()),
+			};
+			new.push(value);
+		};
+	}
+	fn vomit(&self) -> String {
+		self.iter().fold(String::new(), |acc, el| acc + &el.vomit() + ",")
+	}
+}
+pub type Section = HashMap<String, String>;
+pub type SectionError = &'static str;      
+
+pub fn parse_for_props(ini_doc: &str) -> HashMap<String, String> {
+    let mut props: HashMap<String, String> = HashMap::new();
+    let parser = Parser::new(ini_doc).auto_trim(true);
+	let mut last_prop = "".to_string();
+    for item in parser {
+        match item {
+            Item::Section(_) => {}
+            Item::Property(k, v) => {
+                props.insert(k.to_lowercase().into(), v.into());
+				last_prop = k.into();
+            }
+            Item::Blank | Item::Comment(_) => {}
+			Item::Action(v) => {
+				if let Some(old) = props.get_mut(&last_prop) {
+					old.push_str(v);
+				};
+			},
+            Item::Error(err) => panic!("{}", err),
+        }
+    }
+    props
+}
---
+++ advini_derive/src/lib.rs
@@ -0,0 +1,253 @@
+use proc_macro2::{TokenStream, Span}; 
+use quote::{quote, quote_spanned, format_ident, ToTokens};
+use syn::{
+    parse_macro_input, parse_quote, Data, DeriveInput, Fields, GenericParam, Generics, Index, Ident, MetaNameValue, Meta, Expr,
+};
+
+#[proc_macro_derive(Sections, attributes(alias, unused, default_value, inline_parsing))]
+pub fn derive_sections(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+	let input = parse_macro_input!(input as DeriveInput);
+	let generics = add_trait_bounds(input.generics);
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+	let name = input.ident;
+	let (from, into) = trait_body(&input.data, &name);
+	quote!(
+		impl #impl_generics Sections for #name #ty_generics #where_clause {
+			fn from_section(sec: Section) -> Result<(Self, HashMap<String, String>), SectionError> {
+				let prop = sec;
+				#from
+			}
+			fn to_section(&self) -> Section {
+				#into
+			}
+		}
+	).into()
+}
+fn add_trait_bounds(mut generics: Generics) -> Generics {
+    for param in &mut generics.params {
+        if let GenericParam::Type(ref mut type_param) = *param {
+            type_param.bounds.push(parse_quote!(Ini));
+        }
+    }
+    generics
+}
+struct FieldInfo {
+	pub aliases: (Ident, Vec<Ident>),
+	pub used: bool,
+	pub default: Option<syn::Expr>,
+	pub inline: bool,
+	pub ty: syn::Type
+}
+fn to_litstr(ident: &Ident) -> syn::LitStr {
+	syn::LitStr::new(&ident.to_string(), ident.span())
+}
+
+fn trait_body(data: &Data, ident: &Ident) -> (TokenStream, TokenStream) {
+	let mut fields = Vec::new();
+	match data {
+		Data::Enum(_) => { unimplemented!() },
+		Data::Struct(s) => {
+			match &s.fields {
+				Fields::Named(f) => {
+					for field in &f.named {
+						fields.push(FieldInfo {
+							aliases: (field.ident.clone().unwrap(), Vec::new()),
+							used: true,
+							default: None,
+							inline: false,
+							ty: field.ty.clone()
+						});
+						for attr in &field.attrs {
+							if attr.path().is_ident("alias") {
+								match &attr.meta {
+									syn::Meta::List(ml) => {
+										ml.parse_nested_meta(|meta| {
+											fields.last_mut().unwrap().aliases.1.push(meta.path.get_ident().unwrap().clone());
+											Ok(())
+										}).ok();
+									},
+									syn::Meta::Path(p) => {
+										fields.last_mut().unwrap().aliases.1.push(p.get_ident().unwrap().clone());
+									},
+									_ => unimplemented!()
+								}
+							}
+							if attr.path().is_ident("unused") {
+								fields.last_mut().unwrap().used = false;
+							}
+							if attr.path().is_ident("inline_parsing") {         
+							    fields.last_mut().unwrap().inline = true;
+							}                                           
+							if attr.path().is_ident("default_value") {
+								//fields.last_mut().unwrap().default = Some(syn::LitStr::attr.meta.require_name_value().unwrap().value.clone());
+								if let Meta::NameValue(meta) = &attr.meta {
+									if let syn::Expr::Lit(exprlit) = &meta.value {
+										if let syn::Lit::Str(litstr) = &exprlit.lit {
+											 fields.last_mut().unwrap().default = Some(litstr.parse::<Expr>().unwrap());
+										}
+									}
+								}
+							}
+						}
+					}
+				},
+				Fields::Unit => {
+					quote!(
+						#ident
+					);
+				},
+				Fields::Unnamed(_) => {
+					
+				},
+			}
+		},
+		Data::Union(_) => unimplemented!(),
+	}
+	
+	let field_declarations = fields.iter().map(|field| {
+		let ident = &field.aliases.0;
+		let ty = &field.ty;
+		if let Some(default) = &field.default {
+			quote! {
+				let mut #ident : #ty = #default.into();
+			}
+		} else {
+			quote! {
+				let mut #ident : Option<#ty> = None;
+			}
+		}
+	});
+	let match_patterns = fields.iter().map(|field| {
+		let ident = &field.aliases.0;
+		let ty = &field.ty;
+		let idents = field.aliases.1
+			.iter()		
+			.map(to_litstr);
+		let name = to_litstr(&ident);
+		let pattern = if field.aliases.1.is_empty() {
+			quote!( #name )
+		} else {
+			quote!( #name | #(#idents)|* )
+		};
+		let res = if let Some(_) = &field.default {
+			quote!(
+				res
+			)
+		} else {
+			quote!(
+				Some(res)
+			)
+		};
+		let mut ty_ = quote!( #ty );
+		let tystr = ty.to_token_stream().to_string();
+		if tystr.starts_with("Option") {
+			let tystr = syn::LitStr::new(tystr.split_once("<").unwrap().1.rsplit_once(">").unwrap().0, Span::call_site()).parse::<syn::Type>().unwrap();
+			ty_ = quote!( #tystr );
+		}
+		if field.inline {
+			quote!(
+				
+			)
+		} else {
+			quote!(
+				#pattern => #ident = match <#ty_ as Ini>::eat(v) {
+					Ok((res, chars)) => {
+						v = chars;
+						#res.into()
+					},
+					Err(IniParseError::Empty(chars)) => {
+						v = chars;
+						continue;
+					},
+					Err(IniParseError::Error(info)) => {
+						println!("{}", info);
+						continue;
+					}
+				},		
+ 			)
+		}
+	});
+	let name = &ident;
+	let construct_fields = fields
+		.iter()
+		.filter(|f|
+			f.used
+		)
+		.map(|f| {
+			let ident = &f.aliases.0;
+			if let Some(_) = &f.default {
+				quote!(#ident)
+			} else { quote!(#ident.unwrap()) }
+		});
+	let struct_construction = quote! {
+		#name::new( #(#construct_fields),* )
+	};
+	let inlined_fields = fields
+		.iter()
+		.filter(|f|
+			f.inline
+		)
+		.map(|f| {
+			let ident = &f.aliases.0;
+			let ty = &f.ty;
+			quote!(
+				if !remaining.is_empty() {
+					let res = <#ty as Sections>::from_section(remaining).unwrap();
+					(#ident, remaining) = (res.0.into(), res.1);
+				}
+			)
+		});
+	
+	let field_declarations = quote! {
+		#(#field_declarations )*
+		let mut remaining = HashMap::new();
+		for (k, value) in prop.iter() {
+			let mut v = value.chars();
+			match &**k {
+				#(#match_patterns)*
+				any => {
+					remaining.insert(k.clone(), value.clone());
+				}
+			}
+		};
+		#(#inlined_fields)*
+		Ok((#struct_construction, remaining))
+	};
+
+	let to_section = to_section_body(&fields, ident);
+	(field_declarations, to_section )
+}
+
+fn to_section_body(fields: &Vec<FieldInfo>, _ident: &Ident) -> TokenStream {
+	let fields_filtered = fields
+		.iter()
+		.filter(|f| f.used);
+	let fields_declarations = fields_filtered.clone()
+		.map(|f| {
+			let name = to_litstr(&f.aliases.0);
+			let ident = &f.aliases.0;
+			let ty = &f.ty;
+			if f.ty.to_token_stream().to_string().starts_with("Option") {
+				quote_spanned!( Span::call_site() =>
+					if let Some(val) = &self.#ident {
+						section.insert(#name.into(), val.vomit());
+					}
+				)
+			} else if f.inline {
+				quote_spanned!( Span::call_site() =>
+					section.extend(<#ty as Sections>::to_section(&self.#ident));
+				)
+			} else {
+				quote!(
+					section.insert(#name.into(), self.#ident.vomit());
+				)
+			}
+		});
+	quote!(
+		let mut section = HashMap::new();
+		#(
+			#fields_declarations
+		)*
+		section
+	)
+}
